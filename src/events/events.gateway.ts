20dea44273dc86ade78861acc818253025a62150
hena <somnium1108@gmail.com>
2023-05-03 18:33:21 +0900

    [Refactor] remove all console log

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -33,7 +33,7 @@ export class EventsGateway
   private moveValue = 4;
   private maxGoalScore = 5;
 
-  private gameRoom: {[key: string]: any} = {};
+  private gameRoom: { [key: string]: any } = {};
 
   // frame id
   private intervalIds = {};
@@ -46,8 +46,7 @@ export class EventsGateway
   private socketRoomMap = new Map<string, SocketInfo>();
 
   startGame(roomName: string) {
-    function collision(b:BallObject, p: PlayerObject) : boolean
-    {
+    function collision(b: BallObject, p: PlayerObject): boolean {
       const playerTop = p.y;
       const playerBottom = p.y + p.height;
       const playerLeft = p.x;
@@ -58,18 +57,17 @@ export class EventsGateway
       const ballLeft = b.x - b.radius;
       const ballRight = b.x + b.radius;
 
-      
-      return ballRight > playerLeft && ballBottom > playerTop && 
-              ballLeft < playerRight && ballTop < playerBottom;
+
+      return ballRight > playerLeft && ballBottom > playerTop &&
+        ballLeft < playerRight && ballTop < playerBottom;
     }
 
-    function resetBall(ball : BallObject, w : number, h : number) {
-      // console.log(ball, w,h);
+    function resetBall(ball: BallObject, w: number, h: number) {
       ball.x = w / 2;
       ball.y = h / 2;
-  
+
       ball.speed = 5;
-      ball.velocityX = -ball.velocityX; 
+      ball.velocityX = -ball.velocityX;
     }
 
     const setId = setInterval(() => {
@@ -77,78 +75,64 @@ export class EventsGateway
 
       data.ball.x += data.ball.velocityX;
       data.ball.y += data.ball.velocityY;
-  
+
       if (data.ball.y + data.ball.radius > this.canvasH ||
         data.ball.y - data.ball.radius < 0) {
         data.ball.velocityY = -data.ball.velocityY;
       }
-      // console.log(data.left, data.right);
       let player = (data.ball.x < this.canvasW / 2) ? data.left : data.right;
-      
-      // console.log(data.ball.x, data.ball.y, player.x, player.y);
-      if (collision(data.ball, player))
-      {
 
+      if (collision(data.ball, player)) {
         let collidePoint = data.ball.y - (player.y + player.height / 2);
         collidePoint = collidePoint / (player.height / 2);
 
         let angleRad = collidePoint * Math.PI / 4;
-        let direction = (data.ball.x < this.canvasW /  2) ? 1 : -1;
-        data.ball.velocityX = direction *  data.ball.speed * Math.cos(angleRad);
-        data.ball.velocityY =              data.ball.speed * Math.sin(angleRad);
+        let direction = (data.ball.x < this.canvasW / 2) ? 1 : -1;
+        data.ball.velocityX = direction * data.ball.speed * Math.cos(angleRad);
+        data.ball.velocityY = data.ball.speed * Math.sin(angleRad);
 
         data.ball.speed += 0.1;
       }
       // update paddle
-      // console.log(data.left.state, data.right.state)
       if (data.left.state == 1) {
         data.left.y = Math.max(data.left.y - this.moveValue, 0);
-        // console.log(data.left.y, data.left.x, data.right.y, data.right.x)
       }
       else if (data.left.state == 2) {
         data.left.y = Math.min(data.left.y + this.moveValue, this.canvasH - data.left.height);
-        // console.log(data.left.y, data.left.x, data.right.y, data.right.x)
       }
       if (data.right.state == 1) {
         data.right.y = Math.max(data.right.y - this.moveValue, 0);
-        // console.log(data.left.y, data.left.x, data.right.y, data.right.x)
       }
       else if (data.right.state == 2) {
         data.right.y = Math.min(data.right.y + this.moveValue, this.canvasH - data.right.height);
-        // console.log(data.left.y, data.left.x, data.right.y, data.right.x)
       }
-      
 
       // update the score
-      if (data.ball.x - data.ball.radius < 0)
-      {
+      if (data.ball.x - data.ball.radius < 0) {
         data.right.score++;
         resetBall(data.ball, this.canvasW, this.canvasH);
-      } 
-      else if (data.ball.x + data.ball.radius > this.canvasW){
+      }
+      else if (data.ball.x + data.ball.radius > this.canvasW) {
         data.left.score++;
         resetBall(data.ball, this.canvasW, this.canvasH);
       }
       this.server.to(roomName).emit('render', data.left, data.right, data.ball, roomName);
-      
-      
-      if (this.isGameOver(data.left.score, data.right.score, roomName))
-      {
+
+
+      if (this.isGameOver(data.left.score, data.right.score, roomName)) {
         // stop interval and clear
         clearInterval(this.intervalIds[roomName]);
         delete this.intervalIds[roomName];
 
-        console.log("nickname Test", data.left.nick, data.right.nick);
         // socketMap clear
         const remainClients = this.server.sockets.adapter.rooms.get(roomName);
         for (const key of remainClients) {
           this.socketRoomMap.delete(key);
         }
-        console.log("my socket:",this.socketRoomMap);
 
         const gameType = this.gameRoom[roomName].type.flag;
         let winScore, loseScore, winId, loserId;
-        
+
         const winner = data.left.score > data.right.score ? data.left : data.right;
         const loser = data.left.score < data.right.score ? data.left : data.right;
         winScore = winner.score;
@@ -165,15 +149,14 @@ export class EventsGateway
     }, 40);
 
     // set Interval Id => if game end, need to clear setInterval
-    // console.log("Set Id:", setId);
     this.intervalIds[roomName] = setId;
   }
 
   afterInit(server: Server) {
-      
+
 
     console.log('WebSocketGateway initialized');
-    
+
   }
 
   // 연결된 socket이 연결될때 동작하는 함수 - OnGatewayConnection 짝궁
@@ -186,23 +169,19 @@ export class EventsGateway
   // 연결된 socket이 끊어질때 동작하는 함수 - OnGatewayDisconnect 짝궁
   handleDisconnect(client: any, ...args: any[]) {
     // Get roomName and PlayerId that the client belongs to.
-    if (this.socketRoomMap.get(client.id) !== undefined)
-    {
+    if (this.socketRoomMap.get(client.id) !== undefined) {
       const roomName = this.socketRoomMap.get(client.id).roomName;
       const playerId = this.socketRoomMap.get(client.id).playerId;
 
       // Delete client in socketRoomMap because of client leave our webPage
       this.socketRoomMap.delete(client.id);
-      
-            // only player 1 and 2 win
-      if (playerId === 1 || playerId === 2)
-      {
+
+      // only player 1 and 2 win
+      if (playerId === 1 || playerId === 2) {
         let winScore, loseScore, winId, loserId;
         const gameType = this.gameRoom[roomName].type.flag;
         // player 2p win
-        if (playerId === 1)
-        {
-          console.log(roomName, "is winner 2p");
+        if (playerId === 1) {
           this.server.to(roomName).emit('gameover', 2);
           winScore = this.gameRoom[roomName].right.score;
           loseScore = this.gameRoom[roomName].left.score;
@@ -210,11 +189,9 @@ export class EventsGateway
           loserId = this.gameRoom[roomName].left.nick;
         }
         // player 1p win
-        else if (playerId === 2)
-        {
-          console.log(roomName, "is winner 1p");  
+        else if (playerId === 2) {
           this.server.to(roomName).emit('gameover', 1);
-          
+
           winScore = this.gameRoom[roomName].left.score;
           loseScore = this.gameRoom[roomName].right.score;
           winId = this.gameRoom[roomName].left.nick;
@@ -227,28 +204,24 @@ export class EventsGateway
 
 
         // socketRoomMap[roomName] all clear
-        // console.log("Roomssss", this.server.sockets.adapter.rooms.get(roomName));
         const remainClients = this.server.sockets.adapter.rooms.get(roomName);
-        // console.log(typeof(remainClients));
         for (const key of remainClients) {
-          // console.log(key);
           this.socketRoomMap.delete(key);
         }
 
         // Processing Database
         // Alee's TODO
-        console.log("state: Quit", "mapNumber:", gameType, winScore, loseScore, "id:", winId, loserId);
+        // console.log("state: Quit", "mapNumber:", gameType, winScore, loseScore, "id:", winId, loserId);
 
         // remove socket(real socket) room
         this.server.socketsLeave(roomName);
       }
     }
-    else{
+    else {
       console.log("undifined");
     }
 
     delete this.sessionMap[client.id];
-    // console.log(this.sessionMap);
   }
 
   // press Up key
@@ -258,15 +231,11 @@ export class EventsGateway
     @MessageBody() message,
   ) {
     const [room, id] = message;
-    if (id === 1)
-    {
+    if (id === 1) {
       this.gameRoom[room].left.state = 1;
-      console.log(this.gameRoom[room].left);
     }
-    else if (id === 2)
-    {
+    else if (id === 2) {
       this.gameRoom[room].right.state = 1;
-      console.log(this.gameRoom[room].right);
     }
   }
 
@@ -277,31 +246,24 @@ export class EventsGateway
     @MessageBody() data
   ) {
     const [room, id] = data;
-    console.log(room, id);
-    if (id === 1)
-    {
+    if (id === 1) {
       this.gameRoom[room].left.state = 2;
-      console.log(this.gameRoom[room].left);
     }
-    else if (id === 2)
-    {
+    else if (id === 2) {
       this.gameRoom[room].right.state = 2;
-      console.log(this.gameRoom[room].right);
     }
   }
 
   // release Up Key
   @SubscribeMessage('handleKeyRelUp')
   async handleKeyRelUp(
-    @ConnectedSocket() client, 
+    @ConnectedSocket() client,
     @MessageBody() message) {
     const [roomName, id] = message;
-    if (id === 1)
-    {
+    if (id === 1) {
       this.gameRoom[roomName].left.state = 0;
     }
-    else if (id === 2)
-    {
+    else if (id === 2) {
       this.gameRoom[roomName].right.state = 0;
     }
   }
@@ -313,24 +275,19 @@ export class EventsGateway
     @MessageBody() message,
   ) {
     const [roomName, id] = message;
-    if (id === 1)
-    {
+    if (id === 1) {
       this.gameRoom[roomName].left.state = 0;
     }
-    else if (id === 2)
-    {
+    else if (id === 2) {
       this.gameRoom[roomName].right.state = 0;
     }
   }
-  
-  
+
+
 
   // socket의 메시지를 room내부의 모든 이들에게 전달합니다.
   @SubscribeMessage('match')
   async enqueueMatch(@ConnectedSocket() client, @MessageBody() data) {
-    // console.log("socket:", client);
-    console.log("data:", data);
-        
     const queueData = {
       socket: client,
       gameType: data
@@ -351,44 +308,36 @@ export class EventsGateway
       let left;
       let right;
       let gameType;
-      if (this.matchNormalQueue.length >= 2)
-      {
+      if (this.matchNormalQueue.length >= 2) {
         left = this.matchNormalQueue.shift();
         right = this.matchNormalQueue.shift();
         gameType = false;
       }
 
-      if (this.matchExtendQueue.length >= 2)
-      {
+      if (this.matchExtendQueue.length >= 2) {
         left = this.matchExtendQueue.shift();
         right = this.matchExtendQueue.shift();
         gameType = true;
       }
 
       const roomName = randomBytes(10).toString('hex');
-      const newGameObject: GameData = 
-      createGameData(
-        createLeftPlayerObject(),
-        createRightPlayerObject(),
-        createBallObject(),
-        createGameType(gameType),
-      );
-      
+      const newGameObject: GameData =
+        createGameData(
+          createLeftPlayerObject(),
+          createRightPlayerObject(),
+          createBallObject(),
+          createGameType(gameType),
+        );
+
       // nickname add part
-
       this.gameRoom[roomName] = newGameObject;
-      console.log('gameRoom: ', this.gameRoom);
-      console.log('gameRoom[roomName]: ', this.gameRoom[roomName]);
-
-      
-      // TODO roomname 필요
       left.socket.join(roomName); // TODO
       right.socket.join(roomName); // TODO
 
-      const leftInfo: SocketInfo = {roomName, playerId: 1};
-      const rightInfo: SocketInfo = {roomName, playerId: 2};
-      this.socketRoomMap.set(left.socket.id, leftInfo); 
-      this.socketRoomMap.set(right.socket.id, rightInfo); 
+      const leftInfo: SocketInfo = { roomName, playerId: 1 };
+      const rightInfo: SocketInfo = { roomName, playerId: 2 };
+      this.socketRoomMap.set(left.socket.id, leftInfo);
+      this.socketRoomMap.set(right.socket.id, rightInfo);
 
 
       this.server.to(roomName).emit('matchingcomplete', 200, roomName);
@@ -400,10 +349,8 @@ export class EventsGateway
     }
   }
 
-  isGameOver(leftScore : number, rightScore : number, roomName: string) : boolean
-  {
-    if (leftScore >= this.maxGoalScore || rightScore >= this.maxGoalScore)
-    {
+  isGameOver(leftScore: number, rightScore: number, roomName: string): boolean {
+    if (leftScore >= this.maxGoalScore || rightScore >= this.maxGoalScore) {
       if (leftScore >= this.maxGoalScore) {
         this.server.to(roomName).emit('gameover', 1);
       }
@@ -419,23 +366,18 @@ export class EventsGateway
   // param[socketId]
   @SubscribeMessage('cancel queue')
   async cancelQueue(@ConnectedSocket() client, @MessageBody() data) {
-    console.log("cancel queue", client.id);
-    console.log("cancel game type", data);
-
-    // data 정의 X
-    if (data === false)
-    {
-      for(var i = 0; i < this.matchNormalQueue.length; i++){ 
+    if (data === false) {
+      for (var i = 0; i < this.matchNormalQueue.length; i++) {
         console.log(this.matchNormalQueue[i].socket.id, client.id);
-        if (this.matchNormalQueue[i].socket.id === client.id) { 
+        if (this.matchNormalQueue[i].socket.id === client.id) {
           this.matchNormalQueue.splice(i, 1);
           break;
         }
       }
     } else {
-      for(var i = 0; i < this.matchNormalQueue.length; i++){ 
-        if (this.matchExtendQueue[i].socket.id === client.id) { 
-          this.matchExtendQueue.splice(i, 1); 
+      for (var i = 0; i < this.matchNormalQueue.length; i++) {
+        if (this.matchExtendQueue[i].socket.id === client.id) {
+          this.matchExtendQueue.splice(i, 1);
           break;
         }
       }
@@ -446,16 +388,10 @@ export class EventsGateway
   @SubscribeMessage('want observer')
   async WatchingGame(@ConnectedSocket() client, @MessageBody() data) {
     const nickName: string = data;
-    console.log("want Observer", nickName, this.socketRoomMap);
     const sock: SocketInfo = this.socketRoomMap.get(nickName);
-    console.log("sock TEst", sock);
-    if (sock !== undefined)
-    {
-
+    if (sock !== undefined) {
       const roomName: string = sock.roomName;
       client.join(roomName);
-      console.log("client Test", client);
-      console.log("observer", roomName);
       this.server.to(roomName).emit('matchingcomplete', 200, roomName);
       // this.server.to(client.id).emit('game observer', 200);  
       // this.server.to(client.id).emit('isLeft', 3);
@@ -469,70 +405,54 @@ export class EventsGateway
   // sessionMap:[nick, socket]
   private sessionMap = {};
   @SubscribeMessage('Invite Game')
-  async InviteGame(@ConnectedSocket() client, @MessageBody() data)
-  {
+  async InviteGame(@ConnectedSocket() client, @MessageBody() data) {
     const nickName = data;
-    // console.log("tq", data, nickName);
     // 1. Check if your opponent is online or offline
     const socketData = this.sessionMap[nickName];
-    if (socketData === undefined)
-    {
-      this.server.to(client.id).emit('invite message fail');  
-      console.log("1번 펑");
-      return ;
+    if (socketData === undefined) {
+      this.server.to(client.id).emit('invite message fail');
+      return;
     }
 
     // 2. Check if your opponent is playing or spectating
-    if (socketData.state === 'inGame')
-    {
-      this.server.to(client.id).emit('invite message fail');  
-      console.log("2번 펑");
-      return ;
+    if (socketData.state === 'inGame') {
+      this.server.to(client.id).emit('invite message fail');
+      return;
     }
     // 3. return invite complete event
     this.server.to(client.id).emit('invite message complete');
-    // console.log("opp:id:", this.sessionMap[nickName].id)
     this.server.to(socketData.id).emit('invite message', client.id);
   }
 
   @SubscribeMessage('Accept invitation')
-  async InviteOK(@ConnectedSocket() client, @MessageBody() data)
-  {
-
+  async InviteOK(@ConnectedSocket() client, @MessageBody() data) {
     const [nickName, enqueueFlag, gameType] = data;
 
-    // console.log(data);
     // 1. Check if your opponent is online or offline
     const socketData = this.sessionMap[nickName];
-    
-    if (socketData === undefined)
-    {
-      this.server.to(client.id).emit('invite fail');  
-      console.log("InviteOK 1번 실패");
-      return ;
+
+    if (socketData === undefined) {
+      this.server.to(client.id).emit('invite fail');
+      return;
     }
 
     // 2. Check if your opponent is playing or spectating
-    if (socketData.state === 'ingame')
-    {
-      this.server.to(client.id).emit('invite fail');  
-      console.log("InviteOK 2번 실패");
-      return ;
+    if (socketData.state === 'ingame') {
+      this.server.to(client.id).emit('invite fail');
+      return;
     }
     // 3. return invite complete event
     // 3-1. remove my data in WaitingQueue
-    if (enqueueFlag === true)
-    {
-      for(var i = 0; i < this.matchNormalQueue.length; i++){ 
-        console.log(this.matchNormalQueue[i].socket.nickName, client.id);
-        if (this.matchNormalQueue[i].socket.nickName === nickName) { 
+    if (enqueueFlag === true) {
+      for (var i = 0; i < this.matchNormalQueue.length; i++) {
+        if (this.matchNormalQueue[i].socket.nickName === nickName) {
           this.matchNormalQueue.splice(i, 1);
           break;
         }
       }
-      for(var i = 0; i < this.matchNormalQueue.length; i++){ 
-        if (this.matchExtendQueue[i].socket.nickName === nickName) { 
-          this.matchExtendQueue.splice(i, 1); 
+      for (var i = 0; i < this.matchNormalQueue.length; i++) {
+        if (this.matchExtendQueue[i].socket.nickName === nickName) {
+          this.matchExtendQueue.splice(i, 1);
           break;
         }
       }
@@ -542,32 +462,29 @@ export class EventsGateway
     const roomName = randomBytes(10).toString('hex');
 
     // 3-3. create GameObject
-    const newGameObject: GameData = 
-    createGameData(
-      createLeftPlayerObject(),
-      createRightPlayerObject(),
-      createBallObject(),
-      createGameType(gameType),
-    );
+    const newGameObject: GameData =
+      createGameData(
+        createLeftPlayerObject(),
+        createRightPlayerObject(),
+        createBallObject(),
+        createGameType(gameType),
+      );
     this.gameRoom[roomName] = newGameObject;
 
     // 3-4. join room
-    console.log("RoomName Check", roomName);
     client.join(roomName); // TODO
     socketData.join(roomName); // TODO
 
-    const leftInfo: SocketInfo = {roomName, playerId: 1};
-    const rightInfo: SocketInfo = {roomName, playerId: 2};
-    this.socketRoomMap.set(client.id, leftInfo); 
-    this.socketRoomMap.set(socketData.id, rightInfo); 
-    console.log("join", this.socketRoomMap);
+    const leftInfo: SocketInfo = { roomName, playerId: 1 };
+    const rightInfo: SocketInfo = { roomName, playerId: 2 };
+    this.socketRoomMap.set(client.id, leftInfo);
+    this.socketRoomMap.set(socketData.id, rightInfo);
 
     // 3-5. both set id
     this.server.to(roomName).emit('matchingcomplete', 200, roomName);
     this.server.to(client.id).emit('isLeft', 1);
     this.server.to(socketData.id).emit('isLeft', 2);
 
-    console.log("matching 완료");
     this.startGame(roomName);
 
     // 3-6. send complete message
@@ -576,8 +493,7 @@ export class EventsGateway
 
 
   @SubscribeMessage('playerBackspace')
-  async BackClick(@ConnectedSocket() client, @MessageBody() data)
-  {
+  async BackClick(@ConnectedSocket() client, @MessageBody() data) {
     const [roomName, nickName] = data;
 
     console.log(roomName, nickName);
